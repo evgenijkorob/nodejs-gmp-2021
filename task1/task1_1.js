a6ca1254a5e24a3c24497374833bbf21c809be81
Eugene Korobeinikov <evgenij.k.wie@gmail.com>
2021-03-18 00:50:03 +0300

    fix(task1): fixed discussed issues

--- task1/task1_1.js
+++ task1/task1_1.js
@@ -2,10 +2,14 @@ import { Transform } from 'stream';
 
 class ReverseTransform extends Transform {
   _transform(buf, encoding, callback) {
-    const inputStr = buf.toString('utf-8');
-    const reversedStr = inputStr.trim().split('').reverse().join('');
+    try {
+      const inputStr = buf.toString('utf-8');
+      const reversedStr = inputStr.trim().split('').reverse().join('');
 
-    callback(null, reversedStr + '\n');
+      callback(null, reversedStr + '\n');
+    } catch (err) {
+      callback(err);
+    }
   }
 }
 
--- task1/task1_2.js
+++ task1/task1_2.js
@@ -1,29 +1,26 @@
+import { join } from 'path';
 import { createReadStream, createWriteStream } from 'fs';
-import { Transform } from 'stream';
 import { createInterface } from 'readline';
 import csvtojson from 'csvtojson';
 
-const csvFolderPath = './task1/csv';
+const csvFolderName = 'csv';
+const csvFolderPath = join(__dirname, csvFolderName);
 const csvFileName = 'books.csv';
-const csvFilePath = `${csvFolderPath}/${csvFileName}`;
+const csvFilePath = join(csvFolderPath, csvFileName);
 const nowDate = new Date();
 const dateStr = nowDate.toISOString().replace(/:/g, '.');
 const txtFileName = `books_${dateStr}.txt`;
-const txtFilePath = `${csvFolderPath}/${txtFileName}`;
+const txtFilePath = join(csvFolderPath, txtFileName);
 
 const csvFile = createReadStream(csvFilePath, { encoding: 'utf-8' });
 const readlineInterface = createInterface({ input: csvFile });
-const csvByLine = new Transform({
-  transform: (buf, encoding, callback) => {
-    callback(null, buf);
-  }
-});
 const converter = csvtojson({ delimiter: ';' }, { encoding: 'utf-8' });
-const writeToTxt = createWriteStream(txtFilePath, { encoding: 'utf-8' });
+const writeToTxt = createWriteStream(txtFilePath);
+
+readlineInterface.on('line', (line) => converter.write(line + '\n'));
 
 csvFile.on('error', (err) => console.error('read error\n', err.message));
 converter.on('error', (err) => console.error('cvstojson error\n', err.message));
 writeToTxt.on('error', (err) => console.error('write error\n', err.message));
 
-readlineInterface.on('line', (line) => csvByLine.write(line + '\n'));
-csvByLine.pipe(converter).pipe(writeToTxt);
+converter.pipe(writeToTxt);
