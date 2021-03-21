de5533f6aa29935ca7bebf72408953af45692af3
Eugene Korobeinikov <evgenij.k.wie@gmail.com>
2021-03-21 18:16:52 +0300

    fix(task1): fix txt output format

--- task1/task1_2.js
+++ task1/task1_2.js
@@ -14,10 +14,27 @@ const txtFilePath = join(csvFolderPath, txtFileName);
 
 const csvFile = createReadStream(csvFilePath, { encoding: 'utf-8' });
 const readlineInterface = createInterface({ input: csvFile });
-const converter = csvtojson({ delimiter: ';' }, { encoding: 'utf-8' });
+const converter = csvtojson(
+  {
+    delimiter: ';',
+    checkType: true,
+    ignoreColumns: /(amount)/
+  },
+  { encoding: 'utf-8' }
+);
 const writeToTxt = createWriteStream(txtFilePath);
+let isHeaderRow = true;
 
-readlineInterface.on('line', (line) => converter.write(line + '\n'));
+readlineInterface.on('line', (line) => {
+  let resultLine = line;
+
+  if (isHeaderRow) {
+    resultLine = resultLine.toLowerCase();
+    isHeaderRow = false;
+  }
+
+  return converter.write(resultLine + '\n')
+});
 
 csvFile.on('error', (err) => console.error('read error\n', err.message));
 converter.on('error', (err) => console.error('cvstojson error\n', err.message));
